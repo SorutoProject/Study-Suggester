d3e2085ade9bec5c235e407da644a12dd570f635
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-05-14 23:35:50 +0900

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function change(n){
 		main.innerHTML = "現在、項目はありません。";
 	}
 	else if(n=="sc"){
-		main.innerHTML = bl+'<br><a href="a/?=rekishi-sample" class="sl">歴史サンプル問題</a>';
+		main.innerHTML = bl+'<br><a href="a/?q=rekishi-sample" class="sl">歴史サンプル問題</a>';
 	}
 	else if(n=="top"){
 		main.innerHTML = '<a href="javascript:void(0);" onclick="change(\'jp\')" class="sl">国語</a><a href="javascript:void(0);" onclick="change(\'ma\')" class="sl">数学</a><a href="javascript:void(0);" onclick="change(\'sc\')" class="sl">社会</a>';
