bc8c5c6a45a14421377e42321c67367378e9da45
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-05-15 00:22:45 +0900

    update

--- a/study-suggester.js
+++ a/study-suggester.js
@@ -53,7 +53,7 @@ for(var i=0;pair[i];i++) {
 };
 function next(){
 if(answered >= quesnum){
-document.getElementById("question").innerHTML = 'これで問題は終わりです。<br>お疲れ様でした。<br>※出題問題数はトップメニューの設定ボタンから変更できます。<br><a href="../">トップメニューへ</a>';
+document.getElementById("question").innerHTML = 'これで問題は終わりです。<br>お疲れ様でした。<br>※出題問題数はトップメニューの設定ボタンから変更できます。<br><a href="../">トップメニューへ</a><br><a href="javascript:location.reload();">もう一度解く</a>';
 document.getElementById("answer").style.display = "none";
 document.getElementById("ansb").style.display = "none";
 }else{
