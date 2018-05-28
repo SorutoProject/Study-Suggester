ff0b2d74f24d25fe17a8420bf1177dc90d6c52e7
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-05-28 10:45:02 +0900

    update

--- index.js
+++ index.js
@@ -158,3 +158,18 @@ document.getElementById("smodal-ok-button").value = "やめる";
 document.getElementById("answer").style.display = "inline";
 document.getElementById("ansb").style.display = "inline";
 }
+function fs() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  }
+  else {
+    cancelFullScreen.call(doc);
+  }
+}
+
